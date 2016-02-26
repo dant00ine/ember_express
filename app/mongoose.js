e3dd57e9a5a4c0201f9b00997a7d941f393107ac
Daniel T <danthwa@gmail.com>
2016-02-26 11:30:00 -0800

    new branch for stormpath integration

--- app/mongoose.js
+++ app/mongoose.js
@@ -2,6 +2,16 @@
 
 // modules
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 // connect to mongoose database
-mongoose.connect('')
+mongoose.connect('');
+
+var models_path = __dirname + '/models'
+
+// find appropriate model for 
+fs.readdirSync(models_path).forEach(function(file){
+	if(file.indexOf('.js') > 0){
+		require(models_path + '/' + file)
+	}
+})
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,38 @@
 
 module.exports = function(app) {
 
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html');
-	});
-	
+	app
+
+	.get('*', function(req, res) {
+		res.sendFile('./public/index.html');
+	})
+
+	.get('/users', function(request, response){
+		users.index(request, response)
+	})
+
+	.get('/users/new', function(request, response){
+		users.create(request, response)
+	})
+
+	.get('/users/:id', function(request, response){
+		users.show(request, response)
+	})
+
+	.post('/users/:id/edit', function(request, response){
+		users.update(request, response)
+	})
+
+	.post('/users', function(request, response){
+		users.create(request, response)
+	})
+
+	.post('/users/:id/destroy', function(request, response){
+		users.destroy(request, response)
+	})
+
+	.post('users/:id/update', function(request, response){
+		users.update(request, response)
+	})
+
 };
