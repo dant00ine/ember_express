116b66a018ec15f37b135e3d8c1b815aaec3f749
Daniel T <danthwa@gmail.com>
2016-03-01 15:15:50 -0800

    switching branches, will resume grunt development later

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,18 @@
+function config(name){
+	return require('./tasks/' + name + '.js');
+}
+
 module.exports = function(grunt){
 // configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json')
+		concat: config('concat')
 	});
 
+	//load plugins
+	grunt.loadNpmTasks('grunt-contrib-concats');
+
 	//tasks
-	grunt.registerTask('default', []);
+	grunt.registerTask('default', ['concat']);
 
 };
--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,9 @@ var Schema = mongoose.Schema;
 
 var userSchema = mongoose.Schema({
 	name        :String,
+	password	:String,
+	email		:String,
 	created_at  :{ type: Date, default: Date.now }
-})
+});
 
 mongoose.model("User", userSchema);
